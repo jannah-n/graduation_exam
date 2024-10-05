0368b6b9a36d00c1117e95575cacaabad585e499
nakan <anfuku934621@gmail.com>
2024-10-05 12:55:28 +0900

    "正式なusersテーブルを作成する為、rails db:rollback STEP=6を実行し全てのマイグレーションのstatusをdownに変更" "全てのマイグレーションファイルがdownに変更済みなのを確認後に手動で削除" "rails generate migration CreateUsersを実行後、作成されたマイグレーションファイルにusersテーブルのカラムを追記" "rails db:migrateを実行し、マイグレーションを適用後、rails db:migrate:statusを実行し、作成したマイグレーションファイルのstatusがupになっているか確認" "app/models/user.rbに記述していたusers_v3sテーブルを指定する記述を削除"

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_04_105859) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_05_034603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users_v3s", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
