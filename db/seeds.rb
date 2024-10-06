c004e6532c20605f86d79ac0ec3e030146d511ce
nakan <anfuku934621@gmail.com>
2024-10-06 16:01:48 +0900

    "クイズ出題画面の見た目を調整する為、app/assets/stylesheets/application.bootstrap.scssを修正" "app/assets/stylesheets/application.bootstrap.scssの設定を昨晩の状態に戻し、再修正" "データベースに不明なトラブル発生の為、ユーザー情報とクイズ出題内容を順次登録するコマンドをseeds.rbに記述し、rails db:seedを実行"

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,80 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# ユーザーのデータを作成
+users = [
+  { name: 'あんな', email: 'anna@gmail.com', password: '11111111', password_confirmation: '11111111' },
+  { name: 'ひとし', email: 'hitoshi@gmail.com', password: '22222222', password_confirmation: '22222222' }
+]
+
+users.each do |user|
+  User.create(user)
+end
+
+# クイズのデータを作成
+questions = [
+  {
+    content: '「いぬ」はどちらですか??',
+    correct_image: 'correct_image1.jpg',
+    incorrect_image: 'incorrect_image1.jpg',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: '「ちょうちょ」はどちらですか??',
+    correct_image: 'correct_image2.jpg',
+    incorrect_image: 'incorrect_image2.jpg',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: '「かさ」はどちらですか??',
+    correct_image: 'correct_image3.jpg',
+    incorrect_image: 'incorrect_image3.jpg',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: '「しょうぼうしゃ」はどちらですか??',
+    correct_image: 'correct_image4.jpg',
+    incorrect_image: 'incorrect_image4.jpg',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: '「まる」はどちらですか??',
+    correct_image: 'correct_image5.png',
+    incorrect_image: 'incorrect_image5.png',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: '「さんかく」はどちらですか??',
+    correct_image: 'correct_image6.png',
+    incorrect_image: 'incorrect_image6.png',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: '「しかく」はどちらですか??',
+    correct_image: 'correct_image7.png',
+    incorrect_image: 'incorrect_image7.png',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: 'どちらが大きいですか??',
+    correct_image: 'correct_image8.png',
+    incorrect_image: 'incorrect_image8.png',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: 'どちらが小さいですか??',
+    correct_image: 'correct_image9.png',
+    incorrect_image: 'incorrect_image9.png',
+    correct_choice: '選択肢1'
+  },
+  {
+    content: 'どちらが多いですか??',
+    correct_image: 'correct_image10.png',
+    incorrect_image: 'incorrect_image10.png',
+    correct_choice: '選択肢1'
+  }
+]
+
+questions.each do |question|
+  Question.create(question)
+end
